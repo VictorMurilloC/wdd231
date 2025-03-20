04e9612e308bab38ef923f9adc252febc2eb02cc
VictorMurilloC <vmurillocarrasco@byupathway.edu>
2025-03-20 01:50:50 -0500

    asd

---
+++ scripts/getdate.js
@@ -0,0 +1,10 @@
+let yearElement = document.querySelector("#year");
+let lastModifiedElement = document.querySelector("#lastModified");
+
+let today = new Date();
+let lastModifiedDate = new Date(document.lastModified);
+
+yearElement.innerHTML = today.getFullYear();
+
+let formatter = new Intl.DateTimeFormat("en-US", { timeStyle: 'medium', dateStyle: "medium"});
+lastModifiedElement.innerHTML = formatter.format(lastModifiedDate);
